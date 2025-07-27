8730b2f872480f152db998c0901501703567c4a8
stschult <stuart@pegasuslabs.xyz>
2025-07-26 20:23:16 -0700

    Upgrade to Neural voice

--- server.js
+++ server.js
@@ -19,33 +19,33 @@ app.post('/voice', (req, res) => {
   const twiml = new twilio.twiml.VoiceResponse();
   
   twiml.say({
-    voice: 'Polly.Matthew',
+    voice: 'Polly.Neural.Matthew',
     rate: '95%'
   }, 'Hi Jim! It has been a few days since you had an update, so let me give you an overall assessment, followed by the major red flags you need to address.');
   
   twiml.pause({ length: 1 });
   
   twiml.say({
-    voice: 'Polly.Matthew',
+    voice: 'Polly.Neural.Matthew',
     rate: '95%'
   }, DEMO_DATA.jim.overallStatus + ' TechFlow Solutions is your biggest concern right now.');
   
   twiml.pause({ length: 1 });
   
   twiml.say({
-    voice: 'Polly.Matthew',
+    voice: 'Polly.Neural.Matthew',
     rate: '95%'
   }, 'Red flag one: ' + DEMO_DATA.jim.redFlags[0]);
   
   twiml.pause({ length: 1 });
   
   twiml.say({
-    voice: 'Polly.Matthew',
+    voice: 'Polly.Neural.Matthew',
     rate: '95%'
   }, 'Red flag two: ' + DEMO_DATA.jim.redFlags[1]);
   
   twiml.say({
-    voice: 'Polly.Matthew',
+    voice: 'Polly.Neural.Matthew',
     rate: '95%'
   }, 'Thanks for calling, Jim. Focus on TechFlow today!');
   
